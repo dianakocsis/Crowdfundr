7ac1abbdb85bd45832e4d940459c6a3552913030
Diana Kocsis <dlk61@pitt.edu>
2023-12-24 18:20:07 -0500

    make project an erc721, change creator to owner, add custom error

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+contract Project is ERC721 {
 
-contract Project {
-
-    event Transfer(address from, address to, uint tokenId);
     event Success(string title, address projectAddress);
     event Cancel(string title, address projectAddress);
 
@@ -16,20 +14,17 @@ contract Project {
     }
 
     Status public status;
-    address payable public creator; 
-    string public title;
-    uint public goal;               
-    uint public deadline;           
+    address public immutable owner;
+    uint256 public constant timeline = 30 days;
+    uint public goal;
+    uint public deadline;
     uint public idCounter;
-    uint public minimumContribution = 0.01 ether; 
+    uint public minimumContribution = 0.01 ether;
     mapping(address => uint) contributions;
     mapping(uint => address) awards;
     mapping(address => uint) ownedTokensCount;
 
-    modifier onlyCreator {
-        require(msg.sender == creator, "Not creator");
-        _;
-    }
+    error OnlyOwner(address owner);
 
     modifier atStatus(Status _status) {
         require(status == _status, "Cannot be called at this time.");
@@ -42,30 +37,39 @@ contract Project {
         }
         _;
     }
-    
-    /// @notice Sets creator, goal, and deadline (30 days) for project
-    /// @param _creator Creator of project
-    /// @param _goal Amount of funding creator wants to raise for the project
-    constructor(address _creator, string memory _title, uint _goal) {
-        creator = payable(_creator);
-        title = _title;
-        goal = _goal * 1 ether;
-        deadline = block.timestamp + 30 days;
+
+    /// @notice Creates a new Project contract
+    /// @param _owner The owner of the project
+    /// @param _goal The goal of the project
+    /// @param _name The name of the project
+    /// @param _symbol The symbol of the project
+    constructor(address _owner, uint256 _goal, string memory _name, string memory _symbol) ERC721(_name, _symbol) {
+        owner = _owner;
+        goal = _goal;
+        deadline = block.timestamp + timeline;
     }
-    
-    /// @notice Can only contribute if project is still being Funded. 
+
+    /// @notice Modifier to check if the caller is the owner
+    modifier onlyOwner() {
+        if (msg.sender != owner) {
+            revert OnlyOwner(owner);
+        }
+        _;
+    }
+
+    /// @notice Can only contribute if project is still being Funded.
     ///           - Bronze tier is granted to anyone contribution.
     ///           - Silver tier is granted to a total contribution of at least 0.25 ETH.
     ///           - Gold tier is granted to a total contribution of at least 1 ETH.
     function contribute() external checkFailed atStatus(Status.Funding) payable {
         require(msg.value >= minimumContribution, "Need to make a larger contribution");
-        
+
         if (msg.value >= 1 ether && contributions[msg.sender] < 1 ether ) {
             _mint(1, msg.sender);
-        } 
+        }
         else if (msg.value >= .25 ether && contributions[msg.sender] < .25 ether) {
             _mint(2, msg.sender);
-            
+
         }
         else if (contributions[msg.sender] == 0) {
             _mint(3, msg.sender);
@@ -77,9 +81,9 @@ contract Project {
 
         if (address(this).balance >= goal) {
             status = Status.Successful;
-            emit Success(title, address(this));
+            emit Success(name(), address(this));
         }
-    } 
+    }
 
     /// @notice Contributors can get a refund only if the project failed or creator cancelled
     function refund() external checkFailed atStatus(Status.Failed) {
@@ -88,19 +92,19 @@ contract Project {
         contributions[msg.sender] = 0;
         (bool sent, ) = msg.sender.call{value: back}("");
         require(sent, "Failed to send refund");
-    } 
+    }
 
     /// @notice Creators can withdraw a percentage of the funds only if project reached goal before deadline
-    function withdraw(uint percentage) external onlyCreator atStatus(Status.Successful) {
+    function withdraw(uint percentage) external onlyOwner atStatus(Status.Successful) {
         uint remove = (address(this).balance / 100) * percentage;
-        (bool sent, ) = creator.call{value:remove}("");
+        (bool sent, ) = owner.call{value:remove}("");
         require(sent, "Failed to send Ether");
     }
 
     /// @notice Only creators can choose to cancel the project before the 30 days are over
-    function cancel() external onlyCreator atStatus(Status.Funding) {      
+    function cancel() external onlyOwner atStatus(Status.Funding) {
         status = Status.Failed;
-        emit Cancel(title, address(this));
+        emit Cancel(name(), address(this));
     }
 
     /// @notice Retrieves the total contribution of the contributor
@@ -126,47 +130,5 @@ contract Project {
     function getTier(uint tokenId) public pure returns (uint8) {
         uint8 lastBits = uint8(tokenId) % 2 ** 2;
         return lastBits;
-    }
-
-    /// @notice Returns number of owned tokens in address's account
-    /// @param _addr The address that function is finding balance of
-    function balanceOf(address _addr) external view returns (uint) {
-        return ownedTokensCount[_addr];
-    }
-
-    /// @notice Returns the owener of the token id
-    /// @param tokenId The token Id that function is finding owner of
-    function ownerOf(uint tokenId) public view returns (address) {
-        address owner = awards[tokenId];
-        require(owner != address(0), "Token is nonexistent");
-        return awards[tokenId];
-    }
-
-    function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes memory data) external payable {
-        revert();
-    }
-
-    function safeTransferFrom(address _from, address _to, uint256 _tokenId) external payable {
-        revert();
-    }
-    
-    function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
-        revert();
-    }
-
-    function approve(address _approved, uint256 _tokenId) external payable {
-        revert();
-    }
-
-    function setApprovalForAll(address _operator, bool _approved) external {
-        revert();
-    }
-    
-    function getApproved(uint256 _tokenId) external view returns (address) {
-        revert();
-    }
-
-    function isApprovedForAll(address _owner, address _operator) external view returns (bool) {
-        revert();
     }
 }
